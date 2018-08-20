0ced05b7de378086bd6eedeb0d82696bbacb94a9
Adrian Salazar <adrian.salazar7897@gmail.com>
2018-08-20 13:13:15 -0700

    Added Movie Cell

---
+++ MovieApp/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  MovieApp
+//
+//  Created by Adrian Salazar on 8/17/18.
+//  Copyright © 2018 Adrian Salazar. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieApp/NowPlayingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NowPlayingViewController.swift
+//  MovieApp
+//
+//  Created by Adrian Salazar on 8/15/18.
+//  Copyright © 2018 Adrian Salazar. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
