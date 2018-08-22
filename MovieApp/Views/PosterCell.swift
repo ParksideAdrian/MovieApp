6c98e05b4d131ecdc7fca603e217f23bb3d5cdc0
Adrian Salazar <adrian.salazar7897@gmail.com>
2018-08-22 12:20:55 -0700

    Added "superhero" view

---
+++ MovieApp/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  MovieApp
+//
+//  Created by Adrian Salazar on 8/21/18.
+//  Copyright © 2018 Adrian Salazar. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
